c8e40db6c5af8d2bce820fbba67873c8ae9ddbd7
Jatin Karla <jatinkarla8@gmail.com>
2020-10-25 12:08:03 +0530

    Updates

---
+++ precache-manifest.5bff076c5d20c3e017cfe9a56bed54c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38f1a3f88aa9579ecbd18666482e42a",
+    "url": "/Worth-It/index.html"
+  },
+  {
+    "revision": "da68a177f050c22cc422",
+    "url": "/Worth-It/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "e7740df25ff8861760c7",
+    "url": "/Worth-It/static/css/main.eb209bc9.chunk.css"
+  },
+  {
+    "revision": "da68a177f050c22cc422",
+    "url": "/Worth-It/static/js/2.ddd05cf4.chunk.js"
+  },
+  {
+    "revision": "55831aa08ca6be2c84f8bcd71912ef8c",
+    "url": "/Worth-It/static/js/2.ddd05cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7740df25ff8861760c7",
+    "url": "/Worth-It/static/js/main.fd5f259c.chunk.js"
+  },
+  {
+    "revision": "d56963c23782be421219",
+    "url": "/Worth-It/static/js/runtime-main.d684b042.js"
+  },
+  {
+    "revision": "0efb5662ecb290d4a9a50e449d597c20",
+    "url": "/Worth-It/static/media/worth.0efb5662.jpg"
+  }
+]);
