58b6029ca8a348dc50c5ad37cd2bd9fc28870883
Jatin Karla <jatinkarla8@gmail.com>
2020-11-03 00:31:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Worth-It/precache-manifest.56623439c590079a5dfa4f2151f42ba6.js"
+  "/Worth-It/precache-manifest.f17570e5ebdaedaffcae44a45d313ca8.js"
 );
 
 self.addEventListener('message', (event) => {
