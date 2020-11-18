ca0ac91097e074eaf86cebc74184cd6c55585302
Jatin Karla <jatinkarla8@gmail.com>
2020-11-18 15:29:53 +0530

    second commit

--- src/Service.jsx
+++ src/Service.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import worth from './images/worth.jpg'
 import Card from './Card';
 import Sdata from './Sdata';
+import './Service.css';
 
 const Service=()=>{
     return (<>
-    <div className="my-5">
+    <div className="serviceText">
         <h1 className="text-center">Our Services</h1>
     </div>
     <div className="container-fluid mb-5">
