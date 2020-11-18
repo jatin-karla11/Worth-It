2a26b7ebde93053e614fb25bf7b68e242257301b
Jatin Karla <jatinkarla8@gmail.com>
2020-11-18 15:33:53 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainApp from './MainApp';
