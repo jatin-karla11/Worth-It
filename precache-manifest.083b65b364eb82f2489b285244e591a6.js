ba531ac5a4f17e885178790775903b1cbea05511
Jatin Karla <jatinkarla8@gmail.com>
2020-11-19 17:06:05 +0530

    Updates

---
+++ precache-manifest.083b65b364eb82f2489b285244e591a6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0540646e8104f254134a0005f280f8e",
+    "url": "/Worth-It/index.html"
+  },
+  {
+    "revision": "cbeef761f29224fd5479",
+    "url": "/Worth-It/static/css/2.8de70ac3.chunk.css"
+  },
+  {
+    "revision": "ba7a9bb684fa4d9bf20b",
+    "url": "/Worth-It/static/css/main.e9cd4f99.chunk.css"
+  },
+  {
+    "revision": "cbeef761f29224fd5479",
+    "url": "/Worth-It/static/js/2.86c3da56.chunk.js"
+  },
+  {
+    "revision": "742d1fcf409159a4cb6cc3248c6b8ab7",
+    "url": "/Worth-It/static/js/2.86c3da56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7a9bb684fa4d9bf20b",
+    "url": "/Worth-It/static/js/main.8d613f31.chunk.js"
+  },
+  {
+    "revision": "d56963c23782be421219",
+    "url": "/Worth-It/static/js/runtime-main.d684b042.js"
+  },
+  {
+    "revision": "db33b1e1b90979b6ef32e0fd2401757c",
+    "url": "/Worth-It/static/media/Chocolate.db33b1e1.jpg"
+  },
+  {
+    "revision": "babc4391e932904cbe8702d1d83d0380",
+    "url": "/Worth-It/static/media/chocolate1.babc4391.jpg"
+  },
+  {
+    "revision": "e72536eff87a33eae735cca115d10105",
+    "url": "/Worth-It/static/media/dairy.e72536ef.jpg"
+  },
+  {
+    "revision": "5ef8fdeefc6b49f286c9874fbc0ed85b",
+    "url": "/Worth-It/static/media/dc.5ef8fdee.jpg"
+  },
+  {
+    "revision": "3f6bff26d88135779012768763aa5368",
+    "url": "/Worth-It/static/media/grocery1.3f6bff26.jpg"
+  },
+  {
+    "revision": "700cedd98c41bc489018dc54343cb351",
+    "url": "/Worth-It/static/media/md.700cedd9.jpg"
+  },
+  {
+    "revision": "4a0830f96e559e8f38211701e75e0215",
+    "url": "/Worth-It/static/media/san1.4a0830f9.jpg"
+  },
+  {
+    "revision": "0f71bbd666f2d703e7fdc8973e04f537",
+    "url": "/Worth-It/static/media/vindhy.0f71bbd6.jpg"
+  },
+  {
+    "revision": "0efb5662ecb290d4a9a50e449d597c20",
+    "url": "/Worth-It/static/media/worth.0efb5662.jpg"
+  },
+  {
+    "revision": "1b113a816bcef90a021baa49b8041052",
+    "url": "/Worth-It/static/media/worth1.1b113a81.jpg"
+  },
+  {
+    "revision": "25b7ac7fa9fe97f8bce132c195360136",
+    "url": "/Worth-It/static/media/worth2.25b7ac7f.jpg"
+  }
+]);
