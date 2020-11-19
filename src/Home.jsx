e8980d3a7b49397eee532cc25ea9096cd9d36159
Jatin Karla <jatinkarla8@gmail.com>
2020-11-19 17:04:42 +0530

    third commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,30 @@ import React from 'react';
 import worth from './images/worth.jpg';
 import {NavLink} from 'react-router-dom';
 import Common from './Common';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import chocolate from './images/Chocolate.jpg';
+import './Home.css'
+import dc from './images/dc.jpg'
+import md from './images/md.jpg'
+import dairy from './images/dairy.jpg'
+import vindhy from './images/vindhy.jpg'
+import grocery1 from './images/grocery1.jpg'
+
+const responsive = {
+    0: { items: 3 },
+    568: { items: 3 },
+    1024: { items: 6 },
+};
+
+const items = [
+    <div className="item"><img className="slideHome" src={dc}/></div>,
+    <div className="item"><img className="slideHome" src={vindhy}/></div>,
+    <div className="item"><img className="slideHome" src={grocery1}/></div>,
+    <div className="item"><img className="slideHome" src={dairy}/></div>,
+    <div className="item"><img className="slideHome" src={md}/></div>,
+    <div className="item"><img className="slideHome" src={chocolate}/></div>,
+];
 
 const Home=()=>{
     return (<>
@@ -9,8 +33,15 @@ const Home=()=>{
     imgsrc={worth}
     visit="/" 
     btname="Sign-In"/>
-    
-    
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <div className="imgslider"><AliceCarousel mouseTracking items={items}
+        responsive={responsive} disableDotsControls="false" disableButtonsControls="false" />
+    </div>
     </>);
 };
 
