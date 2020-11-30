06755bd87e72c21c85b63799e4310800070efc58
Jatin Karla <jatinkarla8@gmail.com>
2020-11-30 13:32:39 +0530

    third commit

--- src/Modicare.js
+++ src/Modicare.js
@@ -3,7 +3,7 @@ import './Product.css'
 import Product from './Product'
 import './shop.css'
 import modicare from './images/modicare.png'
-import { Paper } from '@material-ui/core';
+
 import './modicare.css'
 import mc1 from './images/mc1.jpg'
 import mc2 from './images/mc2.jpg'
@@ -47,6 +47,61 @@ import mcw8 from './images/mcw8.jpg'
 import mcw9 from './images/mcw9.jpg'
 import mcw10 from './images/mcw10.jpg'
 import mcw11 from './images/mcw11.jpg'
+import mch1 from './images/mch1.jpg'
+import mch2 from './images/mch2.jpg'
+import mch3 from './images/mch3.jpeg'
+import mch4 from './images/mch4.jpg'
+import mch5 from './images/mch5.jpg'
+import mch6 from './images/mch6.jpg'
+import mch7 from './images/mch7.jpg'
+import mch8 from './images/mch8.jpg'
+import mch9 from './images/mch9.jpg'
+import mch10 from './images/mch10.jpg'
+import mch11 from './images/mch11.jpg'
+import mch12 from './images/mch12.jpg'
+import mch13 from './images/mch13.jpg'
+import mch14 from './images/mch14.jpg'
+import mch15 from './images/mch15.jpg'
+import mch16 from './images/mch16.jpg'
+import mch17 from './images/mch17.jpg'
+import mch18 from './images/mch18.jpg'
+import mch19 from './images/mch19.jpg'
+import mch20 from './images/mch20.jpg'
+import mch21 from './images/mch21.jpg'
+import mch22 from './images/mch22.jpg'
+import mch23 from './images/mch23.jpg'
+import mch24 from './images/mch24.jpg'
+import mch25 from './images/mch25.jpg'
+import mch26 from './images/mch26.jpg'
+import mch27 from './images/mch27.jpg'
+import mch28 from './images/mch28.jpg'
+import mch29 from './images/mch29.jpg'
+import mch30 from './images/mch30.jpg'
+import mch31 from './images/mch31.jpg'
+import mch32 from './images/mch32.jpg'
+import mch33 from './images/mch33.jpg'
+import mch34 from './images/mch34.jpg'
+import mch35 from './images/mch35.jpg'
+import mch36 from './images/mch36.jpg'
+import mch37 from './images/mch37.jpg'
+import mch38 from './images/mch38.jpg'
+import mch39 from './images/mch39.jpg'
+import mch40 from './images/mch40.jpg'
+import mch41 from './images/mch41.jpeg'
+import mch42 from './images/mch42.png'
+import mch43 from './images/mch43.jpeg'
+import mch44 from './images/mch44.jpg'
+import mch45 from './images/mch45.jpg'
+import mch46 from './images/mch46.jpg'
+import mch47 from './images/mch47.jpg'
+import mch48 from './images/mch48.jpg'
+import mch49 from './images/mch49.jpg'
+import mch50 from './images/mch50.jpg'
+import mch51 from './images/mch51.jpg'
+import mch52 from './images/mch52.jpg'
+import mch53 from './images/mch53.jpg'
+import mcc1 from './images/mcc1.jpg'
+import mcc2 from './images/mcc2.jpg'
 
 const options = [
     {
@@ -286,8 +341,359 @@ function Modicare() {
                  </div>
         </>:""}
 
-            {(category==="homecare")?<></>:""}
-            {(category==="colors")?<></>:""}
+            {(category==="homecare")?<>
+            <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="FRUVEG FRUIT AND VEGETABLE WASH" 
+            price={288} 
+            image={mcc1}/>
+            <Product 
+            id="975272"
+            title="GERMI BUST MULTI-SURFACE DISINFECTANT SPRAY" 
+            price={270} 
+            image={mcc2}/>
+            
+        </div>
+            </>:""}
+            {(category==="colors")?<>
+            <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="BB CREAM" 
+            price={495} 
+            image={mch1}/>
+            <Product 
+            id="975272"
+            title="GO GLOW 3 IN 1 CLEANSER, MAKEUP REMOVER & HYDRATOR" 
+            price={799} 
+            image={mch2}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="HIGH DEFINITION EYELINER" 
+            price={630} 
+            image={mch3}/>
+            <Product 
+            id="975272"
+            title="INFINITE WHITE UV FAIRNESS PRESSED POWDER WITH SPF 18" 
+            price={425} 
+            image={mch4}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="PRO QUICK DRY NAIL LACQUER" 
+            price={140} 
+            image={mch5}/>
+            <Product 
+            id="975272"
+            title="PRO WHITE ANTI – WRINKLE & NOURISHING SHEET MASK WITH COLLAGEN" 
+            price={199} 
+            image={mch6}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="PRO WHITE SUPER SOOTHING & ANTI – AGEING SHEET MASK WITH ALOE" 
+            price={199} 
+            image={mch7}/>
+            <Product 
+            id="975272"
+            title="SHAPE UP CRYSTAL NAIL FILER – BLACK" 
+            price={175} 
+            image={mch8}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="SHAPE UP CRYSTAL NAIL FILER – PINK" 
+            price={175} 
+            image={mch9}/>
+            <Product 
+            id="975272"
+            title="SINDOOR - REAL MAROON" 
+            price={120} 
+            image={mch10}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="SINDOOR - REAL RED" 
+            price={120} 
+            image={mch11}/>
+            <Product 
+            id="975272"
+            title="SUPER MATTE 16HRS STAY EYE LINER SUPER BLACK – 01" 
+            price={700} 
+            image={mch12}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="SUPER MATTE 16HRS STAY EYE LINER SUPER BLUE – 02" 
+            price={700} 
+            image={mch13}/>
+            <Product 
+            id="975272"
+            title="TOO GOOD TO BE MATTE LIPSTICK 4.2G" 
+            price={359} 
+            image={mch14}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="UC AGE REDEFINING COLLAGEN BOOSTING SERUM 18ML" 
+            price={1299} 
+            image={mch15}/>
+            <Product 
+            id="975272"
+            title="UC AGE REDEFINING DAY CREAM 30G" 
+            price={2299} 
+            image={mch16}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="UC AGE REDEFINING NIGHT CREAM 30G" 
+            price={2299} 
+            image={mch17}/>
+            <Product 
+            id="975272"
+            title="UC AGE REDEFINING PORE MINIMIZING MIST 100ML" 
+            price={999} 
+            image={mch18}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="UC AGE REDEFINING POWER LIFT CLEANSER 80ML" 
+            price={999} 
+            image={mch19}/>
+            <Product 
+            id="975272"
+            title="UC AGE REDEFINING UNDER - EYE RECOVERY CONCENTRATE 12ML" 
+            price={1299} 
+            image={mch20}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="UC CHERRY LIP BALM" 
+            price={199} 
+            image={mch21}/>
+            <Product 
+            id="975272"
+            title="UC EXPERT FIRM & LIFT FOUNDATION" 
+            price={1199} 
+            image={mch22}/>
+            
+        </div>
+        <div className="shop_row">
+            <Product 
+            id="975271" 
+            title="UC FLAWLESS FINISH BEAUTY SPONGE-BLACK" 
+            price={199} 
+            image={mch23}/>
+            <Product 
+            id="975272"
+            title="UC FLAWLESS FINISH BEAUTY SPONGE-PINK" 
+            price={199} 
+            image={mch24}/>
+            
+        </div>
+        <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC HYPNOTEYES EYESHADOW QUAD (NEW MRP)" 
+            price={1400} 
+            image={mch25}/>
+        <Product 
+            id="975274"
+            title="UC INSTANT RADIANCE ILLUMINATING LOTION 30ML" 
+            price={1399} 
+            image={mch26}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC INTENSE STAY 10HRS KAJAL BLACKEST BLACK 001 (NEW MRP)" 
+            price={360} 
+            image={mch27}/>
+        <Product 
+            id="975274"
+            title="UC NAIL POLISH REMOVER (NEW MRP)" 
+            price={198} 
+            image={mch28}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC ORANGE LIP BALM" 
+            price={199} 
+            image={mch29}/>
+        <Product 
+            id="975274"
+            title="UC PRO MICELLAR BI PHASE MAKEUP CLEANSER 150 ML" 
+            price={499} 
+            image={mch30}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC PRO WHITE ANTI – SPOT RADIANCE SERUM" 
+            price={499} 
+            image={mch31}/>
+        <Product 
+            id="975274"
+            title="UC PRO WHITE FRESH RADIANCE DAY CREAM 30G" 
+            price={1599} 
+            image={mch32}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC PRO WHITE FRESH RADIANCE LOTION 30ML" 
+            price={1699} 
+            image={mch33}/>
+        <Product 
+            id="975274"
+            title="UC PRO WHITE REPLENISHING NIGHT CREAM 30G" 
+            price={1599} 
+            image={mch34}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC STRAWBERRY LIP BALM" 
+            price={199} 
+            image={mch35}/>
+        <Product 
+            id="975274"
+            title="UC ULTIMATE LASH MASCARA - BLACK (NEW MRP)" 
+            price={595} 
+            image={mch36}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UC ULTIMATE RADIANCE PRIMER 30ML (NEW MRP)" 
+            price={600} 
+            image={mch37}/>
+        <Product 
+            id="975274"
+            title="UCL GEL NAIL LACQUER" 
+            price={155} 
+            image={mch38}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="UCL NAIL LACQUER" 
+            price={135} 
+            image={mch39}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR CREME GLAM LIPSTICK" 
+            price={495} 
+            image={mch40}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR EXPERT FIRM AND LIFT COMPACT WITH HYALURONIC ACID & UV FILTERS" 
+            price={899} 
+            image={mch41}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR FINE TIP PENCIL SHARPENER" 
+            price={175} 
+            image={mch42}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR INTENSE COLOR EYE PENCIL" 
+            price={535} 
+            image={mch43}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR INTENSE IMPACT LIPSTICK" 
+            price={695} 
+            image={mch44}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR LOVE ME MATTE LIPCOLOR" 
+            price={730} 
+            image={mch45}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR MAKEUP MELTING WIPES" 
+            price={255} 
+            image={mch46}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR NAIL LACQUER" 
+            price={215} 
+            image={mch47}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR PERFECT MATTE LIPSTICK" 
+            price={495} 
+            image={mch48}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR PRO WEAR BROW PENCIL 1.08G" 
+            price={600} 
+            image={mch49}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR TRAVEL BUDDY NAIL LACQUER REMOVER WIPES (ROSE CRUSH)" 
+            price={150} 
+            image={mch50}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR ULTIMATE RADIANCE BLUSH" 
+            price={540} 
+            image={mch51}/>
+        <Product 
+            id="975274"
+            title="URBAN COLOR ULTIMATE RADIANCE COMPACT" 
+            price={750} 
+            image={mch52}/>
+                 </div>
+                 <div className="shop_row">
+        <Product 
+            id="975273"
+            title="URBAN COLOR ULTIMATE RADIANCE FOUNDATION" 
+            price={550} 
+            image={mch53}/>
+        {/* <Product 
+            id="975274"
+            title=""    
+            price={240} 
+            image={modicare}/> */}
+                 </div>
+            </>:""}
             {(category==="watches")?<>
             <div className="shop_row">
         <Product 
