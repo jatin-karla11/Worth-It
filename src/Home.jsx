d83f115f0a6c9049631a2e5bfea5ebfb7680df63
Jatin Karla <jatinkarla8@gmail.com>
2020-12-01 21:21:08 +0530

    third commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -62,7 +62,7 @@ const Home=()=>{
         </div>
     </div>
     <br></br>
-    <Link className="pizzaPaperLink" to="/mc"><Paper className="pizzaPaper" elevation={3} ><img className="homePaperImage" src={modicare}/><div><strong>Contact: 9826434165</strong><br></br><strong>Harsh Arora</strong></div></Paper></Link>
+    <Link className="pizzaPaperLink" to="/mc"><Paper className="pizzaPaper" elevation={3} ><img className="homePaperImage" src={modicare}/><div className="contactHarsh"><strong>Contact: 9826434165</strong><br></br><strong>Harsh Arora</strong></div></Paper></Link>
     <br></br>
     <div className="dog">
     <div className="mddesc">
