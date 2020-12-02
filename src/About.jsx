c4076d95d3ac0b03b30c94cd3c426efa770993e4
Jatin Karla <jatinkarla8@gmail.com>
2020-12-02 11:43:28 +0530

    third commit

--- src/About.jsx
+++ src/About.jsx
@@ -37,13 +37,23 @@ const About=()=>{
             onOpen={toggleDrawer(anchor, true)}
           >
             <div className="swipe">
-            <span className="swipeline">1. Cancellation and return policy is not available as of now.</span><hr></hr>
-            <span className="swipeline">2. Free delivery on order above ₹499.</span><hr></hr>
-            <span className="swipeline">3. For instant delivery on the same day the delivery charge will be ₹50.</span><hr></hr>
-            <span className="swipeline">4. We take precautionary measures at every level of order processing.</span><hr></hr>
-            <span className="swipeline">5. For adding your store to Worth-It contact- 9752763949.</span><hr></hr>
-            <span className="swipeline">6. There is no night service.</span><hr></hr>
-            <span className="swipeline">7. We assure you quality in our services.</span>
+              <center><div className="tcheading">Terms and Conditions</div></center>
+            {/* <span className="swipeline">1. Cancellation and return policy is not available as of now.</span>
+            <span className="swipeline">2. Free delivery on order above ₹499.</span>
+            <span className="swipeline">3. For instant delivery on the same day the delivery charge will be ₹50.</span>
+            <span className="swipeline">4. We take precautionary measures at every level of order processing.</span>
+            <span className="swipeline">5. For adding your store to Worth-It contact- 9752763949.</span>
+            <span className="swipeline">6. There is no night service.</span>
+            <span className="swipeline">7. We assure you quality in our services.</span> */}
+            <ul>
+              <li>Cancellation and return policy is not available as of now.</li>
+              <li>Free delivery on order above ₹499.</li>
+              <li>For instant delivery on the same day the delivery charge will be ₹50.</li>
+              <li>We take precautionary measures at every level of order processing.</li>
+              <li>For adding your store to Worth-It contact- 9752763949.</li>
+              <li>There is no night service.</li>
+              <li>We assure you quality in our services.</li>
+            </ul>
             </div>
           </SwipeableDrawer>
         </React.Fragment>
--- src/Tc.js
+++ src/Tc.js
@@ -9,16 +9,18 @@ function Tc() {
             <center><h2><u>Terms &amp; Conditions!</u></h2></center>
             <br></br>
             <div className="tc">
-            <p><strong>1. The duration of order delivery is specified in respective services.</strong></p>
-            <p><strong>2. Both cancellation and return policy is not available as of now.</strong></p>
-            <p><strong>3. We deliver only in Indore but do checkout some services include out of Indore delivery too.</strong></p>
-            <p><strong>4. Free delivery on order above ₹499 else ₹30 will be the delivery charge.</strong></p>
-            <p><strong>5. For instant delivery on the same day the delivery charge will be ₹50.</strong></p>
-            <p><strong>6. We take precautionary measures at every level of order processing.</strong></p>
-            <p><strong>7. For adding your store to Worth-It contact- 9752763949.</strong></p>
-            <p><strong>8. For any help contact- 9752763949.</strong></p>
-            <p><strong>9. There is no night service.</strong></p>
-            <p><strong>10. We assure you quality in our services.</strong></p>
+                <ul>
+            <li><strong>The duration of order delivery is specified in respective services.</strong></li>
+            <li><strong>Both cancellation and return policy is not available as of now.</strong></li>
+            <li><strong>We deliver only in Indore but do checkout some services include out of Indore delivery too.</strong></li>
+            <li><strong>Free delivery on order above ₹499 else ₹30 will be the delivery charge.</strong></li>
+            <li><strong>For instant delivery on the same day the delivery charge will be ₹50.</strong></li>
+            <li><strong>We take precautionary measures at every level of order processing.</strong></li>
+            <li><strong>For adding your store to Worth-It contact- 9752763949.</strong></li>
+            <li><strong>For any help contact- 9752763949.</strong></li>
+            <li><strong>There is no night service.</strong></li>
+            <li><strong>We assure you quality in our services.</strong></li>
+            </ul>
             </div>
         </div>
     )
