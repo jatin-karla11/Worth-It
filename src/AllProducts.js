b35ad6e2053fc8ab7c244bc73611580169985c61
Jatin Karla <jatinkarla8@gmail.com>
2020-12-12 20:28:08 +0530

    third commit

--- src/AllProducts.js
+++ src/AllProducts.js
@@ -6,9 +6,23 @@ import Product from './Product'
 import './shop.css'
 import SearchIcon from '@material-ui/icons/Search';
 import OpenMenu from './OpenMenu';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    typography: {
+      padding: theme.spacing(2),
+    },
+  }));
+
+  
 
 
 function AllProducts() {
+  
+  
     const [{basket,user1},dispatch]=useStateValue();
     const [products,setProducts]=useState([]);
 
@@ -28,14 +42,44 @@ function AllProducts() {
     const products1=products.filter(product1=>{
         return product1.name.toLowerCase().indexOf(search.toLowerCase())!==-1
     })
-    
+  
+    const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
     return (
         <div className="container-fluid">
             <br></br>
             {/* <marquee>This page is under build!!!</marquee> */}
             {/* <img src={process.env.PUBLIC_URL + '/productImages/jatin.jpg'} /> */}
             <br></br>
-            <div className="searchProducts"><center><input className="searchInput" onChange={(e)=>{setSearch(e.target.value)}} placeholder="Search for product.."/><SearchIcon fontSize="large" className="search"/></center></div>
+            <div className="searchProducts"><center><input className="searchInput" onChange={(e)=>{setSearch(e.target.value)}} placeholder="Search for product.."/><SearchIcon aria-describedby={id} variant="contained" onClick={handleClick} fontSize="large" className="search"/></center></div> 
+            <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>Automatic search is on!</Typography>
+      </Popover>
             <br></br>
             
             <center><OpenMenu/></center>
--- src/UserFeedback.js
+++ src/UserFeedback.js
@@ -2,7 +2,7 @@ import React from 'react'
 import StarRating from './StarRating'
 import './StarRating.css'
 import { useStateValue } from './StateProvider';
-
+ 
 function UserFeedback() {
     const [{basket,user1},dispatch]=useStateValue();
     const name=user1?.email
