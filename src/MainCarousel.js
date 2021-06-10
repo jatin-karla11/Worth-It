96abbdc18ad68c502d3f6429b34b3656e361fe19
Jatin Karla <jatinkarla8@gmail.com>
2021-06-10 10:25:12 +0530

    final commit

--- src/MainCarousel.js
+++ src/MainCarousel.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import worth from './images/worth.jpg';
-import worth1 from './images/worth1.jpg';
-import worth2 from './images/worth2.jpg';
+// import worth from './images/worth.jpg';
+import customer from './images/customer1.png';
+import worth2 from './images/homeworth2.jpg'; 
 
 function MainCarousel() { 
     return (
       <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src={worth} class="d-block w-100" alt=""/>
-        </div>
-        <div class="carousel-item">
-          <img src={worth1} class="d-block w-100" alt=""/>
-        </div>
-        <div class="carousel-item">
           <img src={worth2} class="d-block w-100" alt=""/>
         </div>
+        <div class="carousel-item">
+          <img style={{border : "4px solid teal"}} src={customer} class="d-block w-100" alt=""/>
+        </div>
+        {/* <div class="carousel-item">
+          <img src={worth2} class="d-block w-100" alt=""/>
+        </div> */}
       </div>
       <a class="carousel-control-prev" href="#carousel" data-slide="prev">
         <span class="carousel-control-prev-icon"></span>
